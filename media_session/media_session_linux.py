63a75551041c1655a041791b45f683f91fe593e8
virashu <svladichek@gmail.com>
2024-03-06 11:47:06 +0300

    Update deps

--- media_session/media_session_linux.py
+++ media_session/media_session_linux.py
@@ -1,4 +1,10 @@
 from .media_session import BaseMediaSession
-
+import dbus
 
 class MediaSessionLinux(BaseMediaSession): ...
+
+bus = dbus.SessionBus()
+proxy = bus.get_object('org.mpris.MediaPlayer2.rhythmbox','/org/mpris/MediaPlayer2')
+properties_manager = dbus.Interface(proxy, 'org.freedesktop.DBus.Properties')
+properties_manager.Set('org.mpris.MediaPlayer2.Player', 'Volume', 100.0)
+curr_volume = properties_manager.Get('org.mpris.MediaPlayer2.Player', 'Volume')
