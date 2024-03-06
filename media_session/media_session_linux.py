4fa7d42ee1bf0d471af26d8a74c73e9080b49940
virashu <svladichek@gmail.com>
2024-03-06 22:11:33 +0300

    Update linux

--- media_session/media_session_linux.py
+++ media_session/media_session_linux.py
@@ -1,10 +1,37 @@
-from .media_session import BaseMediaSession
+# from media_session import BaseMediaSession
+import re
 import dbus
 
-class MediaSessionLinux(BaseMediaSession): ...
+# class MediaSessionLinux(BaseMediaSession): ...
 
 bus = dbus.SessionBus()
-proxy = bus.get_object('org.mpris.MediaPlayer2.rhythmbox','/org/mpris/MediaPlayer2')
-properties_manager = dbus.Interface(proxy, 'org.freedesktop.DBus.Properties')
-properties_manager.Set('org.mpris.MediaPlayer2.Player', 'Volume', 100.0)
-curr_volume = properties_manager.Get('org.mpris.MediaPlayer2.Player', 'Volume')
+
+
+"""
+dbus-send --print-reply --dest=org.freedesktop.DBus /org/freedesktop/DBus org.freedesktop.DBus.ListNames 2> /dev/null | sed -r -n '/org.mpris.MediaPlayer2/{s@.*org.mpris.MediaPlayer2.([^"]+).*@\1@g;p}'
+"""
+
+def main():
+
+    names = bus.list_names()
+
+    if not names:
+        return
+
+    players = tuple(filter(lambda x: x.startswith("org.mpris.MediaPlayer2."), names))
+    print(*players, sep="\n")
+
+    index = int(input("Index: "))
+
+    selected = players[index]
+
+    print(selected)
+
+    proxy = bus.get_object(str(selected), "/org/mpris/MediaPlayer2")
+
+    properties_manager = dbus.Interface(proxy, "org.freedesktop.DBus.Properties")
+    metadata = properties_manager.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+    print(metadata)
+
+main()
+
